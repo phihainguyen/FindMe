d87a10c9a133c2baecc713f1ce7ed0fb26254bf6
Phi Hai Nguyen <phihai93@gmail.com>
2019-04-05 17:40:34 -0700

    update

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ require("./app/routing/htmlRoute")(app);
 require("./app/routing/apiRoute")(app);
 
 
-app.post("/api/friend", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var closestFriend = req.body;
-  console.log("CHAR NAME: ", closestFriend.name);
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  closestFriend.routeName = closestFriend.name.replace(/\s+/g, "").toLowerCase();
+// app.post("/api/friend", function(req, res) {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body parsing middleware
+//   var closestFriend = req.body;
+//   console.log("CHAR NAME: ", closestFriend.name);
+//   // Using a RegEx Pattern to remove spaces from newCharacter
+//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   closestFriend.routeName = closestFriend.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(closestFriend);
+//   console.log(closestFriend);
 
-  characters.push(closestFriend);
+//   characters.push(closestFriend);
 
-  res.json(closestFriend);
-});
+//   res.json(closestFriend);
+// });
 
 
 
